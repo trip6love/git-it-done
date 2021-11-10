2fa3aa362b5eb332a81977c25579ff23ae5bf027
trip6love <lanebyers13@gmail.com>
2021-11-10 14:03:59 -0500

    Shell

---
+++ assets/js/homepage.js
@@ -0,0 +1,11 @@
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getUserRepos("");
